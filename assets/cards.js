04e256dfa78cc451194df85a8ec6f490cebb3217
GioKhabu <giokhabu@gmail.com>
2022-07-11 21:59:30 +0400

    feat: update card

--- assets/cards.js
+++ assets/cards.js
@@ -1,35 +1,35 @@
 const mainLinks = [
   {
     word: 'Action (set A)',
-    image: './assets/img/main_dive.jpg'
+    image: './img/main_dive.jpg'
   },
   {
     word: 'Action (set B)',
-    image: './assets/img/main_run.jpg'
+    image: './img/main_run.jpg'
   },
   {
     word: 'Animal (set A)',
-    image: './assets/img/main_cat.jpg'
+    image: './img/main_cat.jpg'
   },
   {
     word: 'Animal (set B)',
-    image: './assets/img/main_giraffe.jpg'
+    image: './img/main_giraffe.jpg'
   },
   {
     word: 'Clothes',
-    image: './assets/img/main_dress.jpg'
+    image: './img/main_dress.jpg'
   },
   {
     word: 'Emotions',
-    image: './assets/img/main_surprised.jpg'
+    image: './img/main_surprised.jpg'
   },
   {
     word: 'Nature',
-    image: './assets/img/1nature.jpg'
+    image: './img/1nature.jpg'
   },
   {
     word: 'Musical Instruments',
-    image: './assets/img/2music.jpg'
+    image: './img/2music.jpg'
   }
 
 ]
@@ -39,400 +39,400 @@ const cards = [
     {
       word: 'cry',
       translation: 'ტირილი',
-      image: './assets/img/cry.jpg',
-      audioSrc: './assets/audio/cry.mp3'
+      image: './img/cry.jpg',
+      audioSrc: './audio/cry.mp3'
     },
     {
       word: 'dance',
       translation: 'ცეკვა',
-      image: './assets/img/dance.jpg',
-      audioSrc: './assets/audio/dance.mp3'
+      image: './img/dance.jpg',
+      audioSrc: './audio/dance.mp3'
     },
     {
       word: 'dive',
       translation: 'ყვინთვა',
-      image: './assets/img/dive.jpg',
-      audioSrc: './assets/audio/dive.mp3'
+      image: './img/dive.jpg',
+      audioSrc: './audio/dive.mp3'
     },
     {
       word: 'draw',
       translation: 'ხატვა',
-      image: './assets/img/draw.jpg',
-      audioSrc: './assets/audio/draw.mp3'
+      image: './img/draw.jpg',
+      audioSrc: './audio/draw.mp3'
     },
     {
       word: 'fish',
       translation: 'თევზაობა',
-      image: './assets/img/fish.jpg',
-      audioSrc: './assets/audio/fish.mp3'
+      image: './img/fish.jpg',
+      audioSrc: './audio/fish.mp3'
     },
     {
       word: 'fly',
       translation: 'ფრენა',
-      image: './assets/img/fly.jpg',
-      audioSrc: './assets/audio/fly.mp3'
+      image: './img/fly.jpg',
+      audioSrc: './audio/fly.mp3'
     },
     {
       word: 'hug',
       translation: 'ჩახუტება',
-      image: './assets/img/hug.jpg',
-      audioSrc: './assets/audio/hug.mp3'
+      image: './img/hug.jpg',
+      audioSrc: './audio/hug.mp3'
     },
     {
       word: 'jump',
       translation: 'ხტომა',
-      image: './assets/img/jump.jpg',
-      audioSrc: './assets/audio/jump.mp3'
+      image: './img/jump.jpg',
+      audioSrc: './audio/jump.mp3'
     }
   ],
   [
     {
       word: 'open',
       translation: 'გაღება',
-      image: './assets/img/open.jpg',
-      audioSrc: './assets/audio/open.mp3'
+      image: './img/open.jpg',
+      audioSrc: './audio/open.mp3'
     },
     {
       word: 'play',
       translation: 'თამაში',
-      image: './assets/img/play.jpg',
-      audioSrc: './assets/audio/play.mp3'
+      image: './img/play.jpg',
+      audioSrc: './audio/play.mp3'
     },
     {
       word: 'point',
       translation: 'ჩვენება',
-      image: './assets/img/point.jpg',
-      audioSrc: './assets/audio/point.mp3'
+      image: './img/point.jpg',
+      audioSrc: './audio/point.mp3'
     },
     {
       word: 'ride',
       translation: 'ტარება',
-      image: './assets/img/ride.jpg',
-      audioSrc: './assets/audio/ride.mp3'
+      image: './img/ride.jpg',
+      audioSrc: './audio/ride.mp3'
     },
     {
       word: 'run',
       translation: 'სირბილი',
-      image: './assets/img/run.jpg',
-      audioSrc: './assets/audio/run.mp3'
+      image: './img/run.jpg',
+      audioSrc: './audio/run.mp3'
     },
     {
       word: 'sing',
       translation: 'სიმღერა',
-      image: './assets/img/sing.jpg',
-      audioSrc: './assets/audio/sing.mp3'
+      image: './img/sing.jpg',
+      audioSrc: './audio/sing.mp3'
     },
     {
       word: 'skip',
       translation: 'ხტომა, გამოტოვება',
-      image: './assets/img/skip.jpg',
-      audioSrc: './assets/audio/skip.mp3'
+      image: './img/skip.jpg',
+      audioSrc: './audio/skip.mp3'
     },
     {
       word: 'swim',
       translation: 'ცურვა',
-      image: './assets/img/swim.jpg',
-      audioSrc: './assets/audio/swim.mp3'
+      image: './img/swim.jpg',
+      audioSrc: './audio/swim.mp3'
     }
   ],
   [
     {
       word: 'cat',
       translation: 'კატა',
-      image: './assets/img/cat.jpg',
-      audioSrc: './assets/audio/cat.mp3'
+      image: './img/cat.jpg',
+      audioSrc: './audio/cat.mp3'
     },
     {
       word: 'chick',
       translation: 'წიწილა',
-      image: './assets/img/chick.jpg',
-      audioSrc: './assets/audio/chick.mp3'
+      image: './img/chick.jpg',
+      audioSrc: './audio/chick.mp3'
     },
     {
       word: 'chicken',
       translation: 'ქათამი',
-      image: './assets/img/chicken.jpg',
-      audioSrc: './assets/audio/chicken.mp3'
+      image: './img/chicken.jpg',
+      audioSrc: './audio/chicken.mp3'
     },
     {
       word: 'dog',
       translation: 'ძაღლი',
-      image: './assets/img/dog.jpg',
-      audioSrc: './assets/audio/dog.mp3'
+      image: './img/dog.jpg',
+      audioSrc: './audio/dog.mp3'
     },
     {
       word: 'horse',
       translation: 'ცხენი',
-      image: './assets/img/horse.jpg',
-      audioSrc: './assets/audio/horse.mp3'
+      image: './img/horse.jpg',
+      audioSrc: './audio/horse.mp3'
     },
     {
       word: 'pig',
       translation: 'ღორი',
-      image: './assets/img/pig.jpg',
-      audioSrc: './assets/audio/pig.mp3'
+      image: './img/pig.jpg',
+      audioSrc: './audio/pig.mp3'
     },
     {
       word: 'rabbit',
       translation: 'კურდღელი',
-      image: './assets/img/rabbit.jpg',
-      audioSrc: './assets/audio/rabbit.mp3'
+      image: './img/rabbit.jpg',
+      audioSrc: './audio/rabbit.mp3'
     },
     {
       word: 'sheep',
       translation: 'ცხვარი',
-      image: './assets/img/sheep.jpg',
-      audioSrc: './assets/audio/sheep.mp3'
+      image: './img/sheep.jpg',
+      audioSrc: './audio/sheep.mp3'
     }
   ],
   [
     {
       word: 'bird',
       translation: 'ჩიტი',
-      image: './assets/img/bird.jpg',
-      audioSrc: './assets/audio/bird.mp3'
+      image: './img/bird.jpg',
+      audioSrc: './audio/bird.mp3'
     },
     {
       word: 'fish',
       translation: 'თევზი',
-      image: './assets/img/fish1.jpg',
-      audioSrc: './assets/audio/fish.mp3'
+      image: './img/fish1.jpg',
+      audioSrc: './audio/fish.mp3'
     },
     {
       word: 'frog',
       translation: 'ბაყაყი',
-      image: './assets/img/frog.jpg',
-      audioSrc: './assets/audio/frog.mp3'
+      image: './img/frog.jpg',
+      audioSrc: './audio/frog.mp3'
     },
     {
       word: 'giraffe',
       translation: 'ჟირაფი',
-      image: './assets/img/giraffe.jpg',
-      audioSrc: './assets/audio/giraffe.mp3'
+      image: './img/giraffe.jpg',
+      audioSrc: './audio/giraffe.mp3'
     },
     {
       word: 'lion',
       translation: 'ლომი',
-      image: './assets/img/lion.jpg',
-      audioSrc: './assets/audio/lion.mp3'
+      image: './img/lion.jpg',
+      audioSrc: './audio/lion.mp3'
     },
     {
       word: 'mouse',
       translation: 'თაგვი',
-      image: './assets/img/mouse.jpg',
-      audioSrc: './assets/audio/mouse.mp3'
+      image: './img/mouse.jpg',
+      audioSrc: './audio/mouse.mp3'
     },
     {
       word: 'turtle',
       translation: 'კუ',
-      image: './assets/img/turtle.jpg',
-      audioSrc: './assets/audio/turtle.mp3'
+      image: './img/turtle.jpg',
+      audioSrc: './audio/turtle.mp3'
     },
     {
       word: 'dolphin',
       translation: 'დელფინი',
-      image: './assets/img/dolphin.jpg',
-      audioSrc: './assets/audio/dolphin.mp3'
+      image: './img/dolphin.jpg',
+      audioSrc: './audio/dolphin.mp3'
     }
   ],
   [
     {
       word: 'skirt',
       translation: 'ქვედაბოლო',
-      image: './assets/img/skirt.jpg',
-      audioSrc: './assets/audio/skirt.mp3'
+      image: './img/skirt.jpg',
+      audioSrc: './audio/skirt.mp3'
     },
     {
       word: 'pants',
       translation: 'შარვალი',
-      image: './assets/img/pants.jpg',
-      audioSrc: './assets/audio/pants.mp3'
+      image: './img/pants.jpg',
+      audioSrc: './audio/pants.mp3'
     },
     {
       word: 'blouse',
       translation: 'ბლუზი',
-      image: './assets/img/blouse.jpg',
-      audioSrc: './assets/audio/blouse.mp3'
+      image: './img/blouse.jpg',
+      audioSrc: './audio/blouse.mp3'
     },
     {
       word: 'dress',
       translation: 'კაბა',
-      image: './assets/img/dress.jpg',
-      audioSrc: './assets/audio/dress.mp3'
+      image: './img/dress.jpg',
+      audioSrc: './audio/dress.mp3'
     },
     {
       word: 'boot',
       translation: 'ბათინკი',
-      image: './assets/img/boot.jpg',
-      audioSrc: './assets/audio/boot.mp3'
+      image: './img/boot.jpg',
+      audioSrc: './audio/boot.mp3'
     },
     {
       word: 'shirt',
       translation: 'პერანგი',
-      image: './assets/img/shirt.jpg',
-      audioSrc: './assets/audio/shirt.mp3'
+      image: './img/shirt.jpg',
+      audioSrc: './audio/shirt.mp3'
     },
     {
       word: 'coat',
       translation: 'პალტო',
-      image: './assets/img/coat.jpg',
-      audioSrc: './assets/audio/coat.mp3'
+      image: './img/coat.jpg',
+      audioSrc: './audio/coat.mp3'
     },
     {
       word: 'shoe',
       translation: 'ფეხსაცმელი',
-      image: './assets/img/shoe.jpg',
-      audioSrc: './assets/audio/shoe.mp3'
+      image: './img/shoe.jpg',
+      audioSrc: './audio/shoe.mp3'
     }
   ],
   [
     {
       word: 'sad',
       translation: 'მოწყენილი',
-      image: './assets/img/sad.jpg',
-      audioSrc: './assets/audio/sad.mp3'
+      image: './img/sad.jpg',
+      audioSrc: './audio/sad.mp3'
     },
     {
       word: 'angry',
       translation: 'გაბრაზებული',
-      image: './assets/img/angry.jpg',
-      audioSrc: './assets/audio/angry.mp3'
+      image: './img/angry.jpg',
+      audioSrc: './audio/angry.mp3'
     },
     {
       word: 'happy',
       translation: 'ბედნიერი',
-      image: './assets/img/happy.jpg',
-      audioSrc: './assets/audio/happy.mp3'
+      image: './img/happy.jpg',
+      audioSrc: './audio/happy.mp3'
     },
     {
       word: 'tired',
       translation: 'დაღლილი',
-      image: './assets/img/tired.jpg',
-      audioSrc: './assets/audio/tired.mp3'
+      image: './img/tired.jpg',
+      audioSrc: './audio/tired.mp3'
     },
     {
       word: 'surprised',
       translation: 'გაკვირვებული',
-      image: './assets/img/surprised.jpg',
-      audioSrc: './assets/audio/surprised.mp3'
+      image: './img/surprised.jpg',
+      audioSrc: './audio/surprised.mp3'
     },
     {
       word: 'scared',
       translation: 'შეშინებული',
-      image: './assets/img/scared.jpg',
-      audioSrc: './assets/audio/scared.mp3'
+      image: './img/scared.jpg',
+      audioSrc: './audio/scared.mp3'
     },
     {
       word: 'smile',
       translation: 'გაღიმება',
-      image: './assets/img/smile.jpg',
-      audioSrc: './assets/audio/smile.mp3'
+      image: './img/smile.jpg',
+      audioSrc: './audio/smile.mp3'
     },
     {
       word: 'laugh',
       translation: 'სიცილი',
-      image: './assets/img/laugh.jpg',
-      audioSrc: './assets/audio/laugh.mp3'
+      image: './img/laugh.jpg',
+      audioSrc: './audio/laugh.mp3'
     }
   ],
   [
     {
       word: 'Cave',
       translation: 'მღვიმე',
-      image: './assets/img/1cave.jpg',
-      audioSrc: './assets/audio/1cave.mp3'
+      image: './img/1cave.jpg',
+      audioSrc: './audio/1cave.mp3'
     },
     {
       word: 'Forest',
       translation: 'ტყე',
-      image: './assets/img/1forest.jpg',
-      audioSrc: './assets/audio/1forest.mp3'
+      image: './img/1forest.jpg',
+      audioSrc: './audio/1forest.mp3'
     },
     {
       word: 'Lake',
       translation: 'ტბა',
-      image: './assets/img/1lake.jpg',
-      audioSrc: './assets/audio/1lake.mp3'
+      image: './img/1lake.jpg',
+      audioSrc: './audio/1lake.mp3'
     },
     {
       word: 'Mountain',
       translation: 'მთა',
-      image: './assets/img/1mountain.jpg',
-      audioSrc: './assets/audio/1Mountain.mp3'
+      image: './img/1mountain.jpg',
+      audioSrc: './audio/1Mountain.mp3'
     },
     {
       word: 'Ocean',
       translation: 'ოკეანე',
-      image: './assets/img/1ocean.jpg',
-      audioSrc: './assets/audio/1Ocean.mp3'
+      image: './img/1ocean.jpg',
+      audioSrc: './audio/1Ocean.mp3'
     },
     {
       word: 'River',
       translation: 'მდინარე',
-      image: './assets/img/1river.jpg',
-      audioSrc: './assets/audio/1river.mp3'
+      image: './img/1river.jpg',
+      audioSrc: './audio/1river.mp3'
     },
     {
       word: 'Rainbow',
       translation: 'ცისარტყელა',
-      image: './assets/img/1rainbow.jpg',
-      audioSrc: './assets/audio/1rainbow.mp3'
+      image: './img/1rainbow.jpg',
+      audioSrc: './audio/1rainbow.mp3'
     },
     {
       word: 'Waterfall',
       translation: 'ჩანჩქერი',
-      image: './assets/img/1waterfall.jpg',
-      audioSrc: './assets/audio/1waterfall.mp3'
+      image: './img/1waterfall.jpg',
+      audioSrc: './audio/1waterfall.mp3'
     }
   ],
   [
     {
       word: 'Drum',
       translation: 'დრამი',
-      image: './assets/img/2drum.jpg',
-      audioSrc: './assets/audio/2drum.mp3'
+      image: './img/2drum.jpg',
+      audioSrc: './audio/2drum.mp3'
     },
     {
       word: 'Flute',
       translation: 'ფლეიტა',
-      image: './assets/img/2flute.jpg',
-      audioSrc: './assets/audio/2flute.mp3'
+      image: './img/2flute.jpg',
+      audioSrc: './audio/2flute.mp3'
     },
     {
       word: 'Guitar',
       translation: 'გიტარა',
-      image: './assets/img/2guitar.jpg',
-      audioSrc: './assets/audio/2guitar.mp3'
+      image: './img/2guitar.jpg',
+      audioSrc: './audio/2guitar.mp3'
     },
     {
       word: 'Harp',
       translation: 'არფა',
-      image: './assets/img/2harp.jpg',
-      audioSrc: './assets/audio/2harp.mp3'
+      image: './img/2harp.jpg',
+      audioSrc: './audio/2harp.mp3'
     },
     {
       word: 'Organ',
       translation: 'ორღანი',
-      image: './assets/img/2organ.jpg',
-      audioSrc: './assets/audio/2organ.mp3'
+      image: './img/2organ.jpg',
+      audioSrc: './audio/2organ.mp3'
     },
     {
       word: 'Piano',
       translation: 'პიანინო',
-      image: './assets/img/2piano.jpg',
-      audioSrc: './assets/audio/2piano.mp3'
+      image: './img/2piano.jpg',
+      audioSrc: './audio/2piano.mp3'
     },
     {
       word: 'Saxophone',
       translation: 'საქსაფონი',
-      image: './assets/img/2saxophone.jpg',
-      audioSrc: './assets/audio/2saxophone.mp3'
+      image: './img/2saxophone.jpg',
+      audioSrc: './audio/2saxophone.mp3'
     },
     {
       word: 'Violin',
       translation: 'ვიოლინო',
-      image: './assets/img/2violin.jpg',
-      audioSrc: './assets/audio/2violin.mp3'
+      image: './img/2violin.jpg',
+      audioSrc: './audio/2violin.mp3'
     }
   ]
 ]
